0481b7772f9ffa9dbad0d5acb679cdb910e32803
skm5786 <sumitthakral3@gmail.com>
2022-09-23 10:37:00 +0530

    Initial Commit

---
+++ DS_lab_college/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DS_lab_college
+//
+//  Created by sumit thakral on 23/09/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
